78f99dd0baa9896acfe560162c6cc3b9138a1f47
Jorman Espinoza <jdaniel.espinoza89@gmail.com>
2017-02-26 15:51:37 -0400

    Sintax modification (litle)

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
 // Adding libraries
 const express = require('express') // Framework
 const bodyParser = require('body-parser') // Middleware
-const app =  express() // Creates the app
-const api = require('./routes')
+const app = express() // Creates the app
+const api = require('./routes') // Calls the routes file
 
 app.use(bodyParser.urlencoded({ extended: false })) // Adding bodyParser
 app.use(bodyParser.json()) // Allow the use of JSON in the messages format
