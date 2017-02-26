09e835998f0adb9de78b3b6752a3bd46fe2f4a03
Jorman Espinoza <jdaniel.espinoza89@gmail.com>
2017-02-26 13:37:28 -0400

    About to finish the API

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 
 const express = require('express')
 const ProductCtrl = require('../controllers/product') // Adding Controller
+const auth = require('../middlewares/auth')
 const api = express.Router()
 
 // Routes
@@ -10,5 +11,9 @@ api.get('/product/:productId', ProductCtrl.getProduct)
 api.post('/product', ProductCtrl.saveProduct)
 api.put('/product/:productId', ProductCtrl.updateProduct)
 api.delete('/product/:productId', ProductCtrl.deleteProduct)
-
+/*
+api.get('/private', auth.isAuth, function(req, res) {
+	res.status(200).send({ message: 'You have access' })
+})
+*/
 module.exports = api
